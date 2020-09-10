2b5b88d6b05a174e5fb010ce0ea3f636b0072508
Naveen ColourMoon <naveen@thecolourmoon.com>
2020-09-10 10:26:21 +0530

    InitialCommit

---
+++ app/src/main/java/com/colourmoon/ppdemo/Config.java
@@ -0,0 +1,5 @@
+package com.colourmoon.ppdemo;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID = "Aexh4tkK1lksPIUCLZjAsS47_6382wCTelEjGCv3AaqGE3dK-EvdqQUAzQZT_Rb-xg_htyBL1KFTUTDt";
+}
