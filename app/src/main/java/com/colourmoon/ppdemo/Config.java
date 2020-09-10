90cdd42d17a422eadf23a731dd6b7e100f05edab
Naveen ColourMoon <naveen@thecolourmoon.com>
2020-09-10 10:28:57 +0530

    MinorChange

--- app/src/main/java/com/colourmoon/ppdemo/Config.java
+++ app/src/main/java/com/colourmoon/ppdemo/Config.java
@@ -1,5 +1,5 @@
 package com.colourmoon.ppdemo;
 
 public class Config {
-    public static final String PAYPAL_CLIENT_ID = "Aexh4tkK1lksPIUCLZjAsS47_6382wCTelEjGCv3AaqGE3dK-EvdqQUAzQZT_Rb-xg_htyBL1KFTUTDt";
+    public static final String PAYPAL_CLIENT_ID = "paypal_sandbox_client_id";
 }
